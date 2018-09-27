8bc65dfdf1e7611c00e8600cd0bbb187b1342a4e
Ericka Silva <erickasilva@Erickas-MacBook-Pro.local>
2018-09-26 21:00:25 -0300

    Adição da launch screen, telas de início, login e tela de registrar

---
+++ app/src/main/java/com/atox/RegisterScreenActivity.java
@@ -0,0 +1,13 @@
+package com.atox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
