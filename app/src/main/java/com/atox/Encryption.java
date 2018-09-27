aabe91ddb55a69d9402f83485f19234220ec498c
Unknown <lucasg17.lima@gmail.com>
2018-09-26 23:14:13 -0300

    inclusão de criptografia, validações na GUI e toast

---
+++ app/src/main/java/com/atox/Encryption.java
@@ -0,0 +1,37 @@
+package com.atox;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encryption {
+    public static String encryptPassword(String inputPassword) throws NoSuchAlgorithmException {
+
+        //Criptografa a senha e a coloca num array de bytes
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] messageDigested = new byte[0];
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            messageDigested = md.digest(inputPassword.getBytes(StandardCharsets.UTF_8));
+        }
+
+        //cria uma String com os hexadecimais do array de bytes criptografado
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : messageDigested) {
+            hexString.append(String.format("%02X", b));
+        }
+
+        String outputPassword = hexString.toString();
+
+        return outputPassword;
+    }
+
+
+    /*Compara 2 Strings (essas Strings são as senhas já
+     * criptografadas pelo método encryptPassword).
+     */
+    public static boolean validatePassword(String password1, String password2) {
+
+        return password1.equals(password2);
+
+    }
+}
--- app/src/main/java/com/atox/LoginScreenActivity.java
+++ app/src/main/java/com/atox/LoginScreenActivity.java
@@ -3,14 +3,33 @@ package com.atox;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.NoSuchAlgorithmException;
+
 
 public class LoginScreenActivity extends AppCompatActivity {
+
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    final int TAMANHO_SENHA = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        mEmailView = (EditText) findViewById(R.id.editTextEmail);
+        mPasswordView = (EditText) findViewById(R.id.editTextPassword);
+        try {
+            attemptLogin();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
     public void goToRegisterScreen(View view){
@@ -19,4 +38,63 @@ public class LoginScreenActivity extends AppCompatActivity {
         startActivity(registerScreen);
 
     }
+    private void attemptLogin() throws NoSuchAlgorithmException {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+            alert("Login ou senha inválida");
+        } else {
+            // chamar classe para validar o login(criptografado) no DB
+            String realPassword = Encryption.encryptPassword(password);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    public boolean isCampoVazio(String texto){
+        return (texto.trim().isEmpty() || TextUtils.isEmpty(texto));
+    }
+
+    private boolean isPasswordValid(String password) {
+
+        return !isCampoVazio(password) && password.length() >= TAMANHO_SENHA;
+    }
+
+    private void alert(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
+
 }
