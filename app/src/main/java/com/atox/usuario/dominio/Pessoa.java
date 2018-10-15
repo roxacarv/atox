d1151bb09f09e01e320dd7164371df0b57ace469
Andre Arruda <andre.luizarruda@ufrpe.br>
2018-10-15 20:27:08 -0300

    Atributos e métodos criados na classe Pessoa

--- app/src/main/java/com/atox/usuario/dominio/Pessoa.java
+++ app/src/main/java/com/atox/usuario/dominio/Pessoa.java
@@ -1,4 +1,58 @@
 package com.atox.usuario.dominio;
 
+import java.util.Date;
+
 public class Pessoa {
+
+    private String nome;
+
+    private Date dataNascimento;
+
+    private String cpf;
+
+    private Endereco endereco;
+
+    private String telefone;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
 }
