a8b6624841fb4e0cb39f3c70901bffd0fc50a054
Andre Arruda <andre.luizarruda@ufrpe.br>
2018-10-15 20:54:14 -0300

    Os atributos e métodos da classe Endereco foram criados.

--- app/src/main/java/com/atox/usuario/dominio/Endereco.java
+++ app/src/main/java/com/atox/usuario/dominio/Endereco.java
@@ -1,4 +1,86 @@
 package com.atox.usuario.dominio;
 
 public class Endereco {
+
+    private String cep;
+
+    private String logradouro;
+
+    private String  numero;
+
+    private String complemento;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String estado;
+
+    private String pais;
+
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
 }
