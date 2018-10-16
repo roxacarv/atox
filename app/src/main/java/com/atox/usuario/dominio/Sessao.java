9d31a9b8aa5e91cb767c6c4ad79b4ea27f8f5e8c
Andre Arruda <andre.luizarruda@ufrpe.br>
2018-10-15 21:26:55 -0300

    Atributos e métodos da classe Sessao foram criados.

--- app/src/main/java/com/atox/usuario/dominio/Sessao.java
+++ app/src/main/java/com/atox/usuario/dominio/Sessao.java
@@ -1,4 +1,24 @@
 package com.atox.usuario.dominio;
 
 public class Sessao {
+
+    private final Usuario USUARIO_ATIVO;
+
+    public Sessao(Usuario usuario_ativo) {
+        USUARIO_ATIVO = usuario_ativo;
+    }
+
+
+    public void iniciarSessao(){
+
+    }
+
+    public void finalizarSessao(){
+
+    }
+
+    public Usuario getUSUARIO_ATIVO() {
+        return USUARIO_ATIVO;
+    }
+
 }
