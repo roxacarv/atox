3d2420be9e739f72f0da94ae572f592a039de5ce
Andre Arruda <andre.luizarruda@ufrpe.br>
2018-10-15 21:20:21 -0300

    Atributos e métodos da classe Login foram criados

--- app/src/main/java/com/atox/usuario/dominio/Login.java
+++ app/src/main/java/com/atox/usuario/dominio/Login.java
@@ -1,4 +1,38 @@
 package com.atox.usuario.dominio;
 
 public class Login {
+
+    private String email;
+
+    private String senha;
+
+
+    public void irParaTelaRegistro(){
+
+    }
+
+    public void logar(){
+
+    }
+
+    public void checarCampos(){
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
 }
