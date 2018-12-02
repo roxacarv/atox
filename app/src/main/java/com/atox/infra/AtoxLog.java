3935ca7f5165b4bc874ff9e336f08a1c79e25116
Andre Arruda <andre.luizarruda@ufrpe.br>
2018-12-02 17:58:16 -0200

    Classe AtoxLog criada

---
+++ app/src/main/java/com/atox/infra/AtoxLog.java
@@ -0,0 +1,73 @@
+package com.atox.infra;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AtoxLog {
+
+
+    private int id;
+
+    private int id_usuario;
+
+    private int acao;
+
+    private String erro;
+
+    private String mensagem;
+
+    private String timeStamp;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getAcao() {
+        return acao;
+    }
+
+    public void setAcao(int acao) {
+        this.acao = acao;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date data) {
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
+        String dateTimeString = formato.format(data);
+
+        this.timeStamp = dateTimeString;
+    }
+}
